a3c179fac7e18cba14026c99425c553e0bf8cf36
Dongsu Park <blackxub@naver.com>
2019-02-06 01:59:51 +0900

    flaskblog

---
+++ blog/db.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from click import command, echo
+from flask_sqlalchemy import SQLAlchemy
+from flask.cli import with_appcontext
+
+
+db = SQLAlchemy()
+
+
+class Post(db.Model):
+    """A blog post."""
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    title = db.Column(db.Text, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    slug = db.Column(db.Text, nullable=False, unique=True)
+
+
+@command("init-db")
+@with_appcontext
+def init_db_command():
+    """Initialize the database."""
+    db.create_all()
+    echo("Initialized the database.")
+
+
+def init_app(app):
+    """Initialize the Flask app for database usage."""
+    db.init_app(app)
+    db.app = app
+    db.create_all()
+    #app.cli.add_command(init_db_command)
